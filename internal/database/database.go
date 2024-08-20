b4e947bc5f2daa6daa383656e1df1fda2efe3f9b
Hien-Trinh <hientrinhminh32@gmail.com>
2024-08-20 11:43:39 +0700

    updated login to create refresh token

--- internal/database/database.go
+++ internal/database/database.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"sort"
 	"sync"
+	"time"
 )
 
 type DB struct {
@@ -14,8 +15,9 @@ type DB struct {
 	mux  *sync.RWMutex
 }
 type DBStructure struct {
-	Chirps map[int]Chirp `json:"chirps"`
-	Users  map[int]User  `json:"users"`
+	Chirps        map[int]Chirp        `json:"chirps"`
+	Users         map[int]User         `json:"users"`
+	RefreshTokens map[int]RefreshToken `json:"refresh_tokens"`
 }
 
 type Chirp struct {
@@ -27,6 +29,12 @@ type User struct {
 	Id       int    `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type RefreshToken struct {
+	Id        int       `json:"id"`
+	Token     string    `json:"token"`
+	ExpiresAt time.Time `json:"expires_at"`
 }
 
 // NewDB creates a new database connection
@@ -92,6 +100,31 @@ func (db *DB) CreateUser(email, password string) (User, error) {
 	}
 
 	return user, nil
+}
+
+// CreateRefreshToken creates a new refresh token and saves it to disk
+func (db *DB) CreateRefreshToken(refresh_token_string string, refresh_token_expires_at time.Time) (RefreshToken, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return RefreshToken{}, err
+	}
+
+	uniqueId := len(dbStructure.RefreshTokens) + 1
+
+	refresh_token := RefreshToken{
+		Id:        uniqueId,
+		Token:     refresh_token_string,
+		ExpiresAt: refresh_token_expires_at,
+	}
+
+	dbStructure.RefreshTokens[refresh_token.Id] = refresh_token
+
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return RefreshToken{}, err
+	}
+
+	return refresh_token, nil
 }
 
 // GetChirps returns all chirps in the database
@@ -193,8 +226,9 @@ func (db *DB) ensureDB() error {
 	flag.Parse()
 	if errors.Is(err, os.ErrNotExist) || *dbg {
 		dbStructure := DBStructure{
-			Chirps: make(map[int]Chirp),
-			Users:  make(map[int]User),
+			Chirps:        make(map[int]Chirp),
+			Users:         make(map[int]User),
+			RefreshTokens: make(map[int]RefreshToken),
 		}
 		return db.writeDB(dbStructure)
 	}
